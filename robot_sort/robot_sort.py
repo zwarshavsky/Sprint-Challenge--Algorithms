c6b7200c6cb84d485dc4afcbb1d9e40e690ec883
Zhenya Warshavsky <zwarshavsky@gmail.com>
2020-03-20 18:42:26 -0400

    more code

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,6 +92,12 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    def move_all_the_way_left(self):
+        while self.can_move_left() == True:
+            self.move_left()
+            print(self._position)
+
+
     def sort(self):
         """
         Sort the robot's list.
@@ -112,15 +118,78 @@ class SortingRobot:
         # replicating the bubble sort using built-in methods in the class
         # first step is to pick up the first item in list
 
-        self.swap_item()
-        for i in range(len(self._list)):
-            if self.can_move_right() == True:
+        self.set_light_on()
+        while self.light_is_on() == True:
+            self.set_light_off()
+            while self.can_move_right() == True:
+                self.swap_item()
                 self.move_right()
-                # if self.compare_item() == -1
-            # else:
-                # while can_move_left() == True:
-                    # self.move_left()
-                # continue
+                # print(self.compare_item())
+                if self.compare_item() is None:
+                    print("held item is None")
+                    self.swap_item()
+                    self.move_right()
+                    print(self._position)
+                elif self.compare_item() == 1:
+                    print("held item is larger than item in front")
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    print("after position:",self._position)
+                    self.set_light_on()
+                    if self.can_move_right() == False:
+                        self.move_all_the_way_left()
+                elif self.compare_item() == -1:
+                    print("held item is smaller than item in front",self._position)
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    print("after position:",self._position)
+                    
+
+
+        #selection sort code
+        # def selection_sort(l=l):
+        #     # loop through n-1 elements
+        #     for i in range(0, len(l) - 1):
+        #         cur_index = i
+        #         smallest_index = cur_index
+        #         for j in range(cur_index, len(l)):
+        #             if l[smallest_index] > l[j]:
+        #                 l[smallest_index],l[j] = l[j],l[smallest_index]
+        #     return l
+
+        # self.swap_item()
+            # for i in range(0, len(robot._list) - 1):
+            #     for j in range(self._position,len(robot._list)):
+        # while self.can_move_right() == True:
+        #     self.swap_item()
+        #     self.move_right()
+        #     # print(self.compare_item())
+        #     if self.compare_item() is None:
+        #         print("held item is None")
+        #         self.swap_item()
+        #         self.move_right()
+        #         print(self._position)
+        #     elif self.compare_item() == 1:
+        #         print("held item is larger than item in front")
+        #         self.swap_item()
+        #         self.move_left()
+        #         self.swap_item()
+        #         self.move_right()
+        #         print("after position:",self._position)
+        #     elif self.compare_item() == -1:
+        #         print("held item is smaller than item in front",self._position)
+        #         self.move_left()
+        #         self.swap_item()
+        #         self.move_right()
+        #         print("after position:",self._position)
+
+
+            
+
+
             
 
 
@@ -130,10 +199,9 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [1,3,2,7,9,4,58,29,18,99]
 
     robot = SortingRobot(l)
-    robot.sort()
-    print(robot._item)
-    # print(robot.can_move_right())
-    # print(robot._list)
+    print(robot.sort())
+    print(robot._list)
